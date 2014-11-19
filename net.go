f007eb5e541da5f2c1912385e5703f8cfc2a7336
Steven Pecht <steven.pecht@xconcepts.net>
2014-11-19 11:14:54 -0500

    Added tests for encoding and decoding message.

--- net.go
+++ net.go
@@ -16,27 +16,48 @@ type messageType uint8
 // The list of available message types.
 const (
 	activateMsg messageType = iota
-	ack
+	ackMsg
+	broadcastMsg
 )
 
 // Messages are sent with the first byte being the message type
 // and a string body that represents JSON that can be decerialized
 // into the appropriate message type.
 type Message struct {
-	Type messageType
-	Data string
+	Type       messageType
+	StringData []string
+	FloatData  []float64
 }
 
 // Encodes a messafe for sending over a tcp connection. Format is:
 // {len in}\n{msgbody}
-func (msg Message) Enconde() (outputMsg string, err error) {
+func (msg Message) Encode() (outputMsg string, err error) {
 	msgBody, err := json.Marshal(msg)
+	if err != nil {
+		log.Println("[ERROR] Failed to encode message: " + err.Error())
+	}
 	outputMsg += string(msgBody) + string('\n')
 	return
 }
 
-type ActivateMsg struct {
-	ActiveNodes []Node
+func Decode(b []byte) (Message, error) {
+	var msg Message
+	err := json.Unmarshal(b, &msg)
+	if err != nil {
+		log.Println("[ERROR] Failed to unmarshal message")
+	}
+
+	return msg, err
+}
+
+func CreateBroadcastMsg(stringData []string, floatData []float64) Message {
+	msg := Message{
+		Type:       broadcastMsg,
+		StringData: stringData,
+		FloatData:  floatData,
+	}
+
+	return msg
 }
 
 // tcpListen listens for and handles incoming connections
@@ -47,24 +68,24 @@ func (c *client) tcpListen() {
 	}
 }
 
-// Activates all pending members
-func (c *client) activatePendingMembers() {
-	// TODO implement locks for this
-	pending_members := *c.pendingMembers
-	activeMembers := make([]Node, len(c.ActiveMembers)+len(pending_members))
+// // Activates all pending members
+// func (c *client) activatePendingMembers() {
+// 	// TODO implement locks for this
+// 	pending_members := *c.pendingMembers
+// 	activeMembers := make([]Node, len(c.ActiveMembers)+len(pending_members))
 
-	activeMembers = append(activeMembers, pending_members...)
+// 	activeMembers = append(activeMembers, pending_members...)
 
-	for _, value := range c.ActiveMembers {
-		activeMembers = append(activeMembers, value)
-	}
+// 	for _, value := range c.ActiveMembers {
+// 		activeMembers = append(activeMembers, value)
+// 	}
 
-	for i := 0; i < len(pending_members); i++ {
-		tcpAddr := pending_members[i].GetTCPAddr()
-		tcp_conn, _ := net.DialTCP("tcp", nil, &tcpAddr)
-		c.sendActivateMessage(tcp_conn, activeMembers)
-	}
-}
+// 	for i := 0; i < len(pending_members); i++ {
+// 		tcpAddr := pending_members[i].GetTCPAddr()
+// 		tcp_conn, _ := net.DialTCP("tcp", nil, &tcpAddr)
+// 		c.sendActivateMessage(tcp_conn, activeMembers)
+// 	}
+// }
 
 // handleConn handles a single incoming TCP connection
 func handleConn(c *net.TCPConn, channel chan Message) {
@@ -78,7 +99,7 @@ func handleConn(c *net.TCPConn, channel chan Message) {
 // Marshal the message and send it over a given TCP connection
 func sendMessage(conn *net.TCPConn, msg Message) error {
 	// Serialize the message
-	msgString, err := msg.Enconde()
+	msgString, err := msg.Encode()
 	if err != nil {
 		return err
 	}
@@ -96,25 +117,21 @@ func recvMessage(conn *net.TCPConn) (Message, error) {
 		log.Println("[ERROR] Failed to read message")
 		return Message{}, err
 	}
-	var msg Message
-	err = json.Unmarshal(b, &msg)
-	if err != nil {
-		log.Println("[ERROR] Failed to unmarshal message")
-	}
+	msg, err := Decode(b)
 	return msg, err
 }
 
 // Sends an activate message with the specified active nodes over the given
 // tcp connection.
-func (c *client) sendActivateMessage(conn *net.TCPConn, activeNodes []Node) error {
-	activateMessage := ActivateMsg{ActiveNodes: activeNodes}
-	b, err := json.Marshal(activateMessage)
-	if err != nil {
-		return err
-	}
-	msg := Message{Type: activateMsg, Data: string(b)}
-	return sendMessage(conn, msg)
-}
+// func (c *client) sendActivateMessage(conn *net.TCPConn, activeNodes []Node) error {
+// 	activateMessage := ActivateMsg{ActiveNodes: activeNodes}
+// 	b, err := json.Marshal(activateMessage)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	msg := Message{Type: activateMsg, Data: string(b)}
+// 	return sendMessage(conn, msg)
+// }
 
 // Called to handle the tcp communication of a join.
 func (c *client) waitAndActivate() (int, error) {
--- net_test.go
+++ net_test.go
@@ -27,8 +27,8 @@ func TestSendRecvMessage(t *testing.T) {
 	remoteConn, _ := net.DialTCP("tcp", nil, listenAddr)
 
 	msg := Message{
-		Type: activateMsg,
-		Data: "Hello World",
+		Type:       activateMsg,
+		StringData: []string{"Hello World"},
 	}
 
 	err = sendMessage(remoteConn, msg)
@@ -38,18 +38,45 @@ func TestSendRecvMessage(t *testing.T) {
 
 	go func() {
 		time.Sleep(1 * time.Second)
-		c <- Message{Data: "ERROR"}
+		c <- Message{StringData: []string{"ERROR"}}
 	}()
 	msgReceived := <-c
-	log.Println("[DEBUG] Recieved: " + msgReceived.Data)
-	if msgReceived.Data == "ERROR" {
+	log.Println("[DEBUG] Recieved: " + msgReceived.StringData[0])
+	if msgReceived.StringData[0] == "ERROR" {
 		t.Error("Timed out!")
 	}
 
 }
 
-func TestUnwrapMessage(t *testing.T) {
-	t.Errorf("Not implemented")
+func TestEncodeDecodeMessage(t *testing.T) {
+	weights := []float64{1.0, 2.5, 3}
+	strings := []string{"hello", "world"}
+	msg := CreateBroadcastMsg(strings, weights)
+	msgString, err := msg.Encode()
+	log.Println("[DEBUG] encoded message: " + msgString)
+	if err != nil {
+		t.Error("Encoding message failed")
+	}
+
+	decodedMsg, err := Decode([]byte(msgString))
+	if err != nil {
+		t.Error("Decoding message failed")
+	}
+	if decodedMsg.FloatData[0] != msg.FloatData[0] ||
+		decodedMsg.FloatData[1] != msg.FloatData[1] ||
+		decodedMsg.FloatData[2] != msg.FloatData[2] {
+		t.Error("Decoded floats not the same")
+	}
+
+	if decodedMsg.StringData[0] != msg.StringData[0] ||
+		decodedMsg.StringData[1] != msg.StringData[1] {
+		t.Error("Decoded strings not the same")
+	}
+
+	if decodedMsg.Type != broadcastMsg {
+		t.Error("Incorrect message type")
+	}
+
 }
 
 func HandleIncomeing(l *net.TCPListener, c chan Message) {
