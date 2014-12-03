a9f8d56b83a7d9a5da5d95e423371875c056f788
Steven Pecht <steven.pecht@xconcepts.net>
2014-12-03 10:46:58 -0500

    Implemented creation, joining, and leaving for list backed member tracker.

--- memberTracker.go
+++ memberTracker.go
@@ -3,7 +3,9 @@
 
 package DUP
 
-import ()
+import (
+	"sync"
+)
 
 type MemberTracker interface {
 	// Get the current memberlist
@@ -21,7 +23,68 @@ type MemberTracker interface {
 
 // Implementation of MemberTracker with a syncronized list
 // as the backing, useful for testing purposes
+// Must be thread safe
 type ListMemberTracker struct {
+	// This map is shared between all member trackers. The key is the Node
+	// name and the value is a bool indicating if the membertracker has joined
+	// the other member trackers
+	memberList     map[string]bool
+	memberListLock *sync.Mutex
+	node           Node
+}
+
+// Creates a new list member tracker
+func New(memberList map[string]bool, lock *sync.Mutex, node Node) ListMemberTracker {
+	tracker := ListMemberTracker{
+		memberList:     memberList,
+		memberListLock: lock,
+		node:           node,
+	}
+
+	tracker.memberListLock.Lock()
+	defer tracker.memberListLock.Unlock()
+	// Initialize this tracker to false
+	tracker.memberList[tracker.node.Name] = false
+
+	return tracker
+}
+
+func (tracker ListMemberTracker) GetMemberList() []string {
+	tracker.memberListLock.Lock()
+	defer tracker.memberListLock.Unlock()
+
+	list := make([]string, len(tracker.memberList))
+	i := 0
+	for k, _ := range tracker.memberList {
+		list[i] = k
+		i++
+	}
+	return list
+}
+
+// Join a member tracker group, for the list backed tracker
+// the target string is unused. There is only a single group that
+// can be active at a time.
+func (tracker ListMemberTracker) Join(target string) error {
+	tracker.memberListLock.Lock()
+	defer tracker.memberListLock.Unlock()
+
+	tracker.memberList[tracker.node.Name] = true
+
+	return nil
+}
+
+// Does nothing for list backed member tracker
+func (tracker ListMemberTracker) Start() error {
+	return nil
+}
+
+func (tracker ListMemberTracker) Leave() error {
+	tracker.memberListLock.Lock()
+	defer tracker.memberListLock.Unlock()
+
+	tracker.memberList[tracker.node.Name] = false
+	return nil
 }
 
 type MemberListMemberTracker struct {
--- memberTracker_test.go
+++ memberTracker_test.go
@@ -1,25 +1,29 @@
 package DUP
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestMemberTracker_Create(t *testing.T) {
-	t.Error("Not implemented.")
-
+func GetListMemberTrackers(num int) []ListMemberTracker {
+	return nil
 }
 
-func TestMemberTracker_Start(t *testing.T) {
+func TestMemberTracker_List(t *testing.T) {
+	assert := assert.New(t)
+	num := 5
+	trackers := GetListMemberTrackers(num)
+	assert.Equal(num, len(trackers))
+
+	// Test Starting and initialization
+	for _, v := range trackers {
+		v.Start()
+		assert.Equal(1, len(v.GetMemberList()))
+	}
+
+	// Test Joining
+
+	// Test Leaving
+
 	t.Error("Not implemented.")
-
-}
-
-func TestMemberTracker_Join(t *testing.T) {
-	t.Error("Not implemented.")
-
-}
-
-func TestMemberTracker_Leave(t *testing.T) {
-	t.Error("Not implemented.")
-
 }
