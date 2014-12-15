c4b02406741f5d6e3615a4ce59b89a7034f570df
Steven Pecht <steven.pecht@xconcepts.net>
2014-12-15 12:06:49 -0500

    Added timing of broadcasts to benchmark.

--- docker.go
+++ docker.go
@@ -7,9 +7,14 @@ import (
 )
 
 func main() {
-	numNodes := 3
+	// Test configurations
+	numNodes := 2
+	numIterations := 10
 
+	// Client data
+	isHeadNode := true
 	clients, err := GoMM.GetTCPClients(1)
+
 	if err != nil {
 		fmt.Printf("Failed to create client: %s", err.Error())
 		return
@@ -21,6 +26,7 @@ func main() {
 		fmt.Printf("This node is head node")
 	} else {
 		fmt.Printf("This node is NOT head node")
+		isHeadNode = false
 		client.Join("10.0.2.15:7946")
 		client.WaitActive()
 	}
@@ -31,4 +37,31 @@ func main() {
 		}
 		time.Sleep(50 * time.Millisecond)
 	}
+
+	if isHeadNode {
+		stringData := []string{"Hello", "World"}
+		floatData := []float64{2.0, 48182.2}
+
+		start := time.Now()
+		for i := 0; i < numIterations; i++ {
+			clients[0].Broadcast(stringData, floatData)
+			<-client.BroadcastChannel
+		}
+		elapsed := time.Since(start)
+		fmt.Println("Benchmakr took", elapsed, "for", numIterations, "iterations")
+		fmt.Println("Average seconds per iteration:", elapsed.Seconds()/float64(numIterations))
+	} else {
+		<-client.BroadcastChannel
+		start := time.Now()
+		for i := 0; i < numIterations-2; i++ {
+			<-client.BroadcastChannel
+		}
+		elapsed := time.Since(start)
+		fmt.Println("Benchmakr took", elapsed, "for", numIterations-2, "iterations")
+		fmt.Println("Average seconds per iteration:", elapsed.Seconds()/float64(numIterations-2))
+	}
+
+	// Let all of the messages send
+	time.Sleep(1 * time.Second)
+
 }
